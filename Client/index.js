6ccda09c60a58666d360d402dd82bf1f736b7664
Stylianos Thomas <stylianos.thomas@gmail.com>
2020-11-17 14:53:42 +0000

    fully connected frontend to backend

--- Client/index.js
+++ Client/index.js
@@ -1,5 +1,4 @@
 const newPost=document.getElementById('originalPosts');
-const newReply=document.getElementsByTagName('')
 const myForm=document.getElementById('newPostForm');
 myForm.addEventListener('submit', postOriginal);
 
@@ -8,27 +7,52 @@ displayTimeline();
 function displayTimeline(){
   fetch('http://localhost:3000/blogpost')
     .then(r => r.json())
-    .then(appendpost)
-    .catch(console.warn)
+    .then(displayPosts)
+    .catch(err => console.warn(`Caught problem at position 1: ${err}`))
 };
 
-function appendpost(data){
-  data.timeline.forEach(appendfeeds);
+function displayPosts(data){
+  data.timeline.forEach(appendPost);
 };
 
-function appendfeeds(timeline){
+function appendPost(timeline){
   const newLiPost = document.createElement('li');
-  newLiPost.textContent = `${timeline.post}`
+  newLiPost.textContent = `${timeline.post}`;
   newPost.append(newLiPost);
+  //this is where we add the function for reacts to orginal posts
   timeline.replies.forEach(reply => displayReply(reply));
+  newReplyForm(timeline);
 };
 
 function displayReply(data) {
   const newReplyThread = document.createElement('ul');
   const newReply = document.createElement('li');
-  newReply.textContent = `${data}`
-  newReplyThread.append(newReply)
+  newReply.textContent = `${data}`;
+  newReplyThread.append(newReply);
+  //this is where we add the function for reacts to replies
   newPost.appendChild(newReplyThread);
+};
+
+function newReplyForm(timeline) {
+  const replyContainer = document.createElement('div');
+
+  const replyForm = document.createElement('form');
+  replyForm.addEventListener("submit", postReply);
+
+  const replyTextBox = document.createElement('textarea');
+  const entryId = timeline.id;
+  replyTextBox.setAttribute("id", `${entryId}`);
+  replyTextBox.setAttribute("name", "newReply");
+  replyTextBox.setAttribute("placeholder", "reply to this thread here");
+
+  const replyButton = document.createElement('input');
+  replyButton.setAttribute("type", "submit");
+  replyButton.setAttribute("value", "reply");
+
+  replyForm.append(replyTextBox);
+  replyForm.append(replyButton);
+  replyContainer.append(replyForm);
+  newPost.append(replyContainer);
 };
 
 function postOriginal (e) {
@@ -42,12 +66,34 @@ function postOriginal (e) {
     },
     body: JSON.stringify({
       post:postText,
-      replies:""
+      replies:[]
     })
   };
 
-  fetch('http://localhost:3000/blogpost', options)
+  fetch('http://localhost:3000/newpost', options)
     .then(r => r.json())
-    .then(appendfeeds)
-    .catch(console.warn)
+    .then(appendPost)
+    .catch(err => console.warn(`Caught problem at position 2: ${err}`))
+};
+
+function postReply(e) {
+  e.preventDefault();
+  const replyId = e.target.newReply.id;
+  const replyText=e.target.newReply.value;
+
+  const options = {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      id:replyId,
+      replies:replyText
+    })
+  };
+
+  fetch('http://localhost:3000/newreply', options)
+    .then(r => r.json())
+    .then(appendPost)
+    .catch(err => console.warn(`Caught problem at position 3: ${err}`))
 };
