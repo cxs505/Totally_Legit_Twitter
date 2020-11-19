5e2d0f2b274bd5632251e25a0e6447931fb4746e
cxs505 <charlie-s@hotmail.co.uk>
2020-11-19 14:16:42 +0000

    client test

--- Server/test/clientsuite.js
+++ Server/test/clientsuite.js
@@ -1,7 +1,27 @@
 
-const expect = require('chai').expect;
+const puppeteer = require('puppeteer');
+
 const rewire = require('rewire');
-const index=rewire("../../Client/index.js")
+const index=rewire("../../Client/index.js");
+const expect = require('chai').expect;
+
+
+// import puppeteer from 'puppeteer';
+
+// test('less than 150 characters submitted', async()=>{
+//     let characters = "i’m sorry to hear that your grandma passed away yeah i actually am like obviously i'm i'm going to be upset about that innit yeah and hadiqa you know the only thing is yeah is that no one can force me to be your mate or anything yeah and i don't want to be mates wiv you alright";
+//     const browser= await puppeteer.launch();
+//     const page= await browser.newPage();
+//     const app = 'file:///C:/Users/madeleinemartin/Downloads/FutureProof/Totally_Legit_Twitter/Client/index.html'
+//     await page.goto(app);
+
+//     await page.click('textarea#newPost');
+//     await page.type('textarea#newPost',characters);
+//     await page.click('input#newPostButton');
+//     let newPostInputError= await page.$eval('input#newPostButton',()=>btn.value );
+//     expect(newPostInputError).toBe('invalid')
+//     await browser.close();
+// })
 
 
 describe('userAction', () => {
@@ -23,12 +43,12 @@ describe('userAction', () => {
     })
 });
 
-// describe('fetchGif',()=>{
-//     let fetchGif = index.__get__('fetchGif');
-//     let postText='monkey';
-//     let giphyApiKey = "8x0VRgzjaGPEBptzrtAvSOeWVu6Lxqrb";
-//     let url =`https://api.giphy.com/v1/gifs/random?tag=${postText}&api_key=${giphyApiKey}&limit=1"`;
-//     it('should be a function', () => {
-//         expect(fetchGif('monkey')).to.equal(fetch(url));
-//     });
+// // describe('fetchGif',()=>{
+// //     let fetchGif = index.__get__('fetchGif');
+// //     let postText='monkey';
+// //     let giphyApiKey = "8x0VRgzjaGPEBptzrtAvSOeWVu6Lxqrb";
+// //     let url =`https://api.giphy.com/v1/gifs/random?tag=${postText}&api_key=${giphyApiKey}&limit=1"`;
+// //     it('should be a function', () => {
+// //         expect(fetchGif('monkey')).to.equal(fetch(url));
+// //     });
 // })
